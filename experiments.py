d52537583bde3feecca6519d3c9df7c9452ce3f0
Pinkos1 <Pinkos2003@gmail.com>
2025-12-05 16:40:22 -0500

    added bot times

--- experiments.py
+++ experiments.py
@@ -10,6 +10,10 @@ from graphs import build_k_nearest_graph
 from algorithms import dijkstra, astar
 
 
+
+import time
+import matplotlib.pyplot as plt
+
 def run_experiments():
 
     test_ns = [100, 500, 1000, 2000, 5000] ## Will change over time ##
@@ -42,3 +46,30 @@ def run_experiments():
 
             # find s and t
             s, t = find_source_and_target(points)
+
+
+             # dijkstra time 
+            start = time.time()
+            d_val = dijkstra(adj, s, t)
+            end = time.time()
+            dij_time = end - start
+
+            print("Dijkstra time:", dij_time)
+            dijkstra_times.append(dij_time)
+
+            #astar time
+            start = time.time()
+            a_val = astar(adj, points, s, t)
+            end = time.time()
+            a_time = end - start
+
+            print("A* time:", a_time)
+            astar_times.append(a_time)
+
+            ns_list.append(n)
+
+        # save for plotting
+        results[k] = (ns_list, dijkstra_times, astar_times)
+
+    return results
+
